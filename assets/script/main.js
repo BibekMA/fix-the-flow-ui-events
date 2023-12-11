5ef4eca251c6d5d3716f5772e9c3f0bc848864b4
BibekMA <bibekmainali29@gmail.com>
2023-12-11 12:11:52 +0100

    Click
    Button 1: click

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,23 @@
-let interaction = document.querySelector('a:nth-of-type(12)')
 
-interaction.addEventListener('click', jumpHandler)
-interaction.addEventListener('animationend', jumpHandler)
-
+/*
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+var button = document.querySelector("button");
+var element = document.querySelector("div");
+
+button.addEventListener("click", function(){
+  element.classList.toggle("yay")
+})
+
+*/
+
+
+
+let frontend = document.querySelector('a:nth-of-type(1)')
+
+frontend.addEventListener('click', function(){
+  frontend.classList.toggle('frontend')
+})
+
