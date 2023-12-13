31ec382116ece6da7d1428a0087295d7a9d7657f
BibekMA <bibekmainali29@gmail.com>
2023-12-13 10:06:03 +0100

    update
     btn 3, 4 en 5

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,14 +3,47 @@
 let frontend = document.querySelector('a:nth-of-type(1)')
 
 frontend.addEventListener('click', function(){
-  frontend.classList.toggle('frontend')
+  frontend.classList.toggle('btn1')
 })
 
 
 let design = document.querySelector('a:nth-of-type(2)')
 
 design.addEventListener('dblclick', function(){
-  design.classList.toggle('design')
+  design.classList.toggle('btn2')
 })
+
+
+let and = document.querySelector('a:nth-of-type(3)')
+
+and.addEventListener('mouseover', function (){
+  and.classList.toggle('btn3')
+})
+
+let development = document.querySelector('a:nth-of-type(4)')
+
+development.addEventListener('mouseleave', function (){
+  development.classList.toggle('btn4')
+})
+
+let sprint = document.querySelector('a:nth-of-type(5)')
+
+document.addEventListener('keydown', key1)
+
+function key1(event){
+  if (event.key === "1") {
+    sprint.classList.toggle("btn5")
+  }
+}
+
+let fix = document.querySelector('a:nth-of-type(6)')
+
+document.addEventListener('keyup', key2)
+
+function key2(event){
+  if (event.key === "2") {
+    fix.classList.toggle('btn6')
+  }
+}
 
 
